38cacb1a5ab5849e4a528903b9d8626d5c07528c
Nahid Khan <nahidkhanniloy31@gmail.com>
2026-05-02 15:28:24 +0600

    added code

---
+++ main.py
@@ -0,0 +1,17 @@
+#print("hello word")
+#hello i am nahid
+"""hello thus is multiline comment"""
+
+# x = 12j
+# print(type(x))
+
+# fruits = ["apple", "banana", "cherry"]
+# print(fruits)
+# numbers = range(5)  # 0 to 4
+# print(numbers)
+
+# a = "Nahid Khan"
+# print(a[6::1])
+a= "12"
+# a= int(a)
+print(type(a))
